df8bea041e94400861ed9e815b96c55a9f750cd9
kanishk466 <95866249+kanishk466@users.noreply.github.com>
2022-01-07 09:58:29 +0530

    Update painter_partition_problem.cpp

--- painter_partition_problem.cpp
+++ painter_partition_problem.cpp
@@ -3,21 +3,29 @@
 using namespace std;
 bool isPossible(int arr[], int n, int m, int mid)
 {
-    int painterCount = 1;
-    int partSum = 0;
+    int painterCount = 1;//suru me humare pass painter 1 hoga
+    int partSum = 0;//yahan pr partiion sum naam ka variable taaki hum partion ka sum store kara sake
+    //
     for (int i = 0; i < n; i++)
     {
-        if (partSum+arr[i]<=mid)
+        if (partSum+arr[i]<=mid)//check karenge ki partition  or array of index ka sum agar mid ki value se chota hai to //
+            //aap partion or array of index ko jod do or usko partsum me store kara lo
         {
             partSum+=arr[i];
         }
+        //upar waali condition fail hone me hum 
+        //painter ki value badha denge //
+        
+  
         else
-        {
+        {   // pahle humari painter ki value 1 thi to  ab wah  2 ho jayegi 
             painterCount++;
+            //check karenge ki agar painter ki value input painter ki value se jyada hai or agara array of index ki value jyada hai mid se to return false kar do.
             if (painterCount>m || arr[i]>mid)
             {
                 return false;
             }
+            //upar waali condition agar fail ho jaati hai tab aap array of index ki value partition me store kara lo
             else
             {
               partSum =arr[i];
@@ -26,29 +34,34 @@ bool isPossible(int arr[], int n, int m, int mid)
     }
     return true;
 }
+//is question me hum search space logic use kar rahe hai taaki hum binary search laga sake
 int painterPartition(int arr[], int n, int m)
 {
+    //yahan pr hum initialise kar rahe start of index  
     int s = 0;
     int sum = 0;
+    //humne ek sum naam ka variable banaya hai jo ki 0 store kia hai
+    //aur ek loop cahla dia hai jo ki array of index or sum ki value ko add karega or sum me store karega
+    //kyu ? humne sum ko array of index joda hai, kyu ki humara end is array me sum of array hoga
     for (int i = 0; i < n; i++)
     {
         sum += arr[i];
     }
     int e = sum;
     int mid = s + (e - s) / 2;
-    int ans = -1;
-    while (s <= e)
+    int ans = -1;//isko hum mid ki value store karne ke liye banaye haia
+    while (s <= e)//humara loop tab tak chalega tab tak humara s ki value se badi ho jaye
     {
-        if (isPossible(arr, n, m, mid))
-        {
+        if (isPossible(arr, n, m, mid))//possible solution me hummid ki value store kara lenge
+        {//or mid ko left me le jayenge
             ans = mid;
             e = mid - 1;
         }
         else
-        {
+        {//agar possible solution nhi hai to mid ko right me le jao
             s = mid + 1;
         }
-        mid = s + (e - s) / 2;
+        mid = s + (e - s) / 2;//mid ko phir se upadte kar do // or ans return kar do
     }
     return ans;
 }
